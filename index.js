3df6595911fd28648dbd52450b46211c0438ae94
seanpmaxwell <spmaxwell7@gmail.com>
2025-02-08 09:34:23 -0800

    update chunk append

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 
-// Mock database
+// **** Setup Mock database **** //
+
 const db = new Map([
   ['users', [
     { id: 1, name: 'joe' },
@@ -11,7 +12,12 @@ const db = new Map([
 ])
 
 
-// Create the server
+// **** Create the server **** //
+
+/**
+ * Note "chunk" is a BufferInputStream so we do "body += chunk" to concatenate 
+ * the chunks as they come in.
+ */
 const server = http.createServer((req, res) => {
 
   // Fetch users
@@ -22,23 +28,26 @@ const server = http.createServer((req, res) => {
 
   // Add user
   } else if (req.method === 'POST' && req.url === '/users') {
-    let respObj;
+    let body = '';
     req.on('data', chunk => {
-      const incomingData = JSON.parse(chunk.toString());
-      const currentUsers = db.get('users');
-      currentUsers?.push(incomingData);
-      respObj = currentUsers;
+      body += chunk;
     });
     req.on('end', () => {
+      const incomingData = JSON.parse(body),
+        currentUsers = db.get('users');
+      currentUsers?.push(incomingData);
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(respObj));
+      res.end(JSON.stringify(currentUsers));
     });
 
   // Update user
   } else if (req.method === 'PUT' && req.url === '/users') {
-    let respObj;
+    let body = '';
     req.on('data', chunk => {
-      const incomingData = JSON.parse(chunk.toString()),
+      body += chunk;
+    });
+    req.on('end', () => {
+      const incomingData = JSON.parse(body),
         currentUsers = db.get('users') ?? [];
       for (const user of currentUsers) {
         if (user.id === incomingData.id) {
@@ -46,19 +55,19 @@ const server = http.createServer((req, res) => {
           break;
         }
       }
-      respObj = currentUsers;
-    });
-    req.on('end', () => {
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(respObj));
+      res.end(JSON.stringify(currentUsers));
     });
 
   // Delete a user
   } else if (req.method === 'DELETE' && req.url === '/users') {
-    let respObj;
+    let body = '';
     req.on('data', chunk => {
-      const incomingData = JSON.parse(chunk.toString()),
-        currentUsers = db.get('users') ?? [];
+      body += chunk;
+    });
+    req.on('end', () => {
+      const incomingData = JSON.parse(body),
+      currentUsers = db.get('users') ?? [];
       let index = -1;
       for (let i = 0; i < currentUsers.length; i++) {
         if (currentUsers[i].id === incomingData.id) {
@@ -67,11 +76,8 @@ const server = http.createServer((req, res) => {
         }
       }
       currentUsers.splice(index, 1);
-      respObj = currentUsers;
-    });
-    req.on('end', () => {
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(respObj));
+      res.end(JSON.stringify(currentUsers));
     });
 
   // Not found error
@@ -81,8 +87,27 @@ const server = http.createServer((req, res) => {
   }
 });
 
-// Start listening
+
+// **** Start listening **** //
+
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+});
+
+
+// **** Close server when process exits **** //
+
+process.on('SIGTERM', async () => {
+  console.log('SIGTERM received')
+  server.close(() => {
+    console.log('server closed successfully');
+  });
+});
+
+process.on('SIGINT', async () => {
+  console.log('SIGINT received')
+  server.close(() => {
+    console.log('server closed successfully');
+  });
 });
